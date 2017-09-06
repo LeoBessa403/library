407de58716675d0bfa4d7b7fe1084ad726001190
LeoBessa403 <leonardomcbessa@gmail.com>
2017-09-06 13:01:50 -0300

    Alteração da Estrutura do padrão do projeto

--- Conn/AbstractEnum.class.php
+++ Conn/AbstractEnum.class.php
@@ -3,8 +3,20 @@
 class AbstractEnum
 {
 
-    public function __construct()
+    private static $descricao = [];
+
+    public static function getDescriptionByValue($value)
     {
+        //Replace para tirar a quebra de linha, caso a descricao seja grande demais.
+        return str_replace("\n", "", static::$descricao[$value]);
+    }
+
+    public function getDescription($value = null)
+    {
+        if (!empty($value)) {
+            return static::$descricao[$value];
+        }
+        return static::$descricao;
     }
 
 }

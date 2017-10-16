23607c01db74500ccefb80531fb56d067da7b88b
LeoBessa403 <leonardomcbessa@gmail.com>
2017-10-16 11:25:03 -0200

    Manter Consumidor

--- Conn/AbstractModel.class.php
+++ Conn/AbstractModel.class.php
@@ -132,7 +132,10 @@ class AbstractModel
                 $this->PesquisaTodosNv3($obj, $obj2);
             } else {
                 $novoMetodo = $this->getMetodo($obj::CHAVE);
-                $todos = $this->PesquisaTodosNv1($campo['Entidade'], $campo['Campo'], $obj->$novoMetodo());
+                $todos = null;
+                if(count($obj->$novoMetodo())){
+                    $todos = $this->PesquisaTodosNv1($campo['Entidade'], $campo['Campo'], $obj->$novoMetodo());
+                }
                 $obj->$metodoSet($todos);
                 $this->PesquisaTodosNv3($obj, $obj2);
             }

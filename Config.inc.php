761eb4bca29a43b4c741b8934ba29e3f73680290
LeoBessa403 <leonardomcbessa@gmail.com>
2019-05-02 12:07:59 -0300

    j

--- Config.inc.php
+++ Config.inc.php
@@ -140,7 +140,7 @@ define('PASTA_ENTIDADES', PASTA_RAIZ . ADMIN . '/Entidade/');
 define('PASTA_MODEL', PASTA_RAIZ . ADMIN . '/Model/');
 define('PASTA_CLASS', PASTA_RAIZ . ADMIN . '/Class/');
 define('PASTA_SEVICE', PASTA_RAIZ . ADMIN . '/Service/');
-define('PARTIAL_SITE', PASTA_RAIZ . SITE . '/partial/');
+define('PARTIAL_SITE', PASTA_RAIZ . SITE . '/Partial/');
 define('PARTIAL_ADMIN', PASTA_RAIZ . ADMIN . '/partial/');
 define('PARTIAL_LIBRARY', PASTA_RAIZ . 'library/Partial/');
 define('PASTA_UPLOADS', PASTA_RAIZ . 'uploads/');
